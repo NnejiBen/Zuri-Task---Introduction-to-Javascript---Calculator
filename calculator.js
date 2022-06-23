c6e5982d251626b44a9c1d72a85672d8afc94bf3
NnejiBen <nnejiben@gmail.com>
2022-06-23 10:16:25 +0100

    Initial commit

---
+++ calculator.js
@@ -0,0 +1,27 @@
+// simple prompt calculator
+
+// take operator input
+const operator = prompt('Enter operator ( either +, -, * or / ): ');
+
+// take operand input
+const number1 = parseFloat(prompt('Enter first number: '));
+const number2 = parseFloat(prompt('Enter second number: '));
+
+// execute result
+let result;
+
+if (operator == '+') {
+    result = number1 + number2;
+}
+else if (operator == '-') {
+    result = number1 - number2;
+}
+else if (operator == '*') {
+    result = number1 * number2;
+}
+else {
+    result = number1 / number2;
+}
+
+// display result
+prompt(`${number1} ${operator} ${number2} = ${result}`);
